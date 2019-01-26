67f9d309970a6926655843b23130eff1e9a8cd83
alfonsobries <alfonsobries@gmail.com>
2019-01-25 19:36:27 -0600

    Init cv

---
+++ nuxt.config.js
@@ -0,0 +1,65 @@
+module.exports = {
+  /*
+  ** Headers of the page
+  */
+  head: {
+    titleTemplate: '%s | alfonsobribiesca',
+    htmlAttrs: { lang: 'en' },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Alfonso Bribiesca Cv' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Load global CSS
+  */
+  css: ['@/assets/css/main.css'],
+  /*
+  ** Load nuxt modules
+  */
+  modules: [
+    'nuxt-purgecss'
+  ],
+  /*
+  ** PurgeCSS
+  ** https://github.com/Developmint/nuxt-purgecss
+  */
+  purgeCSS: {},
+  /*
+  ** This option is given directly to the vue-router Router constructor
+  */
+  router: {
+    base: '',
+    linkActiveClass: 'is-active'
+  },
+  /*
+  ** Customize the progress bar color
+  */
+  loading: { color: '#3B8070' },
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
++    ** Extract CSS
++    */
+    extractCSS: true,
+    /*
+    ** Run ESLint on save
+    */
+    extend (config, { isDev, isClient }) {
+      if (isDev && isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  }
+}
